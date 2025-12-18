52f8e97f1fcc9208230c13695a86c953243bc4f2
Muhammed Mustafa Demirhan <muhammedmustafademirhan@gmail.com>
2025-12-18 18:25:20 +0300

    feat(author): add AuthorController

--- KutuphaneAPI/Controllers/AuthorController.cs
+++ KutuphaneAPI/Controllers/AuthorController.cs
@@ -1,4 +1,5 @@
-﻿using KutuphaneService.Interfaces;
+﻿using KutuphaneCore.Entities;
+using KutuphaneService.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,11 +14,69 @@ namespace KutuphaneAPI.Controllers
         {
             _authorService = authorService;
         }
+
         [HttpGet]
         public IActionResult GetAll()
         {
             var authors = _authorService.ListAll();
+
+            if (authors == null)
+                return BadRequest("Yazar bulunamadı.");
+
             return Ok(authors);
+        }
+
+        [HttpGet("GetByName")]
+        public IActionResult GetByName(string name)
+        {
+            if (name == null)
+                return BadRequest("Ad alanı boş bırakılamaz.");
+
+            var result = _authorService.GetByName(name);
+
+            if (result == null)
+                return BadRequest("Yazar bulunamadı.");
+
+            return Ok(result);
+        }
+
+        [HttpGet("GetById")]
+        public IActionResult GetById(int id)
+        {
+            if (id == null)
+                return BadRequest("Geçerli bir Id değeri giriniz.");
+
+            var author = _authorService.GetById(id);
+
+            if (author == null)
+                return BadRequest("Yazar bulunamadı.");
+
+            return Ok(author);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var result = _authorService.Delete(id);
+
+            if (result == null)
+                return BadRequest("Silme işlemi başarısız oldu.");
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]Author author)
+        {
+            if (author == null)
+                return BadRequest("Yazar bilgileri boş olamaz.");
+
+            var result = _authorService.Create(author);
+
+            if (result == null)
+                return BadRequest("Yazar oluşturulamadı.");
+
+            return Ok(result);
         }
     }
 }
